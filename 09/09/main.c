18f7181e40378fed55519434a7de1d8283ae5577
yun9017 <yun9017ie@naver.com>
2025-10-30 16:06:49 +0900

    3

--- 09/09/main.c
+++ 09/09/main.c
@@ -6,22 +6,23 @@
 //
 
 #include <stdio.h>
-#define SIZE 5 //전처리. size 모두 5로 바꿔줌. 매크로
+#define SIZE 5 
 
-int main(void) {
-    int i,average;
-    int sum;
-    int grade[SIZE];
-    
-    sum = 0; //누적해서 연산할 땐 무조건 초기화해주기
-    for(i=0;i<SIZE;i++)
+int main(void)
+{
+    int i;
+    int a[SIZE] = {1,2,3,4,5};
+    int b[SIZE] = {1,2,3,4,6};
+    int flag_same =1;
+
+    for(i=0; i<SIZE; i++)
     {
-        printf("학생 성적을 입력하세요: ");
-        scanf("%d", &grade[i]); //scanf에는 항상 &붙이기
-        sum += grade[i];
+        if (a[i] != b[i])
+        {
+            printf("%i-th element is diff,\n",i);
+            flag_same = 0;
+        }
     }
-    
-    average = sum/SIZE;
-    printf("성적 평균: %d\n",average);
+    printf("Are a and b the same? %d\n", flag_same);
     return 0;
 }
